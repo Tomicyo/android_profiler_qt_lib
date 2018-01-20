f24aff2e04ba7d174694f38d7c31e6d404d41981
alexqzhou <alexqzhou@tencent.com>
2018-01-20 18:26:46 +0800

    update adb command

--- test/perfd_connect.cpp
+++ test/perfd_connect.cpp
@@ -2,6 +2,8 @@
 
 int main(int argc, const char* argv[])
 {
+    // for android o 
+    // adb forward tcp:12389 localabstract:AndroidStudioProfiler
     android::profiler::PerfDClient c("localhost:12389");
     auto profiler = c.GetProfilerService();
     std::string version;
